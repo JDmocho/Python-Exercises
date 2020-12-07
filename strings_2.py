18c0f088bedcaa3962a9f03da4c8041daa745334
JDmocho <joanna.dmocho@gmail.com>
2020-12-07 19:17:38 +0100

    chore: strings exercise

---
+++ strings_2.py
@@ -0,0 +1,27 @@
+# Create a path
+
+drive = 'C:\\'
+folder = 'PythonUdemy\\'
+file = 'myscript.py'
+
+path = drive + folder + file
+print(path)
+
+# Use an apostrophe in a string
+
+print("It's a good idea")
+print('It\'s a good idea')
+
+# Writing on a new line
+text = "You know, you come from nothing \nYou're going back to nothing \nWhat have you lost? Nothing"
+print(text)
+
+# Print function
+
+number = '500'
+
+print(number)
+print(int(number) + 1)
+print(number + str(50))
+print(type(number))
+print(type(int(number) + 500))
